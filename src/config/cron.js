89c08dd1981941c46261f44b76f3edaf4d2b7011
Guillaume Garcia <guigarciafr@gmail.com>
2022-02-13 20:50:44 +0100

    Cron tab retrieve prices

--- src/config/cron.js
+++ src/config/cron.js
@@ -1,5 +1,20 @@
 import cron from "node-cron";
+import fetchPrices from '../services/marketService';
+import getDatabase from '../db/mongo';
+
+/**
+ * Update prices documents in MongoDB 
+ * 
+ */
 
 cron.schedule("1 * * * * *", async () => {
-  console.log("running a task every minute");
+    const priceList = await fetchPrices();
+    const db = await getDatabase();
+    const db_prices = db.collection("prices");
+    db_prices.deleteMany();
+    const options = { ordered: true };
+    const result = await db_prices.insertMany(priceList, options);
+    console.log(`${result.insertedCount} documents were inserted`);
 });
+
+export default cron;
