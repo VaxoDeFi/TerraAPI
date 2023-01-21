a321ad154259461769c9ca42d65bec9fe7b30ac5
Guillaume G <macadition@gmail.com>
2023-01-21 23:02:58 +0100

    update gitignore

--- dist/services/alchemy.js
+++
@@ -1,7 +0,0 @@
-import { Network, Alchemy } from "alchemy-sdk";
-import env from "../env";
-const settings = {
-  apiKey: env.ALCHEMY_KEY,
-  network: Network.ETH_MAINNET
-};
-export const alchemy = new Alchemy(settings);
